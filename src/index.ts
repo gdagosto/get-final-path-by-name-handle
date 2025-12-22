3edc0a757e368fbb050c380a60c149471811f13c
Gabriel d`Agosto <gabrieldagosto97@gmail.com>
2025-12-22 19:09:41 -0300

    new version

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ const require = createRequire(import.meta.url);
 
 const addon = require("./index.node");
 
-export function getFinalPathByNameHandle(nameHandle: number): string {
+export function getFinalPathByNameHandle(nameHandle: string): string {
   return addon.getFinalPathByNameHandle(nameHandle);
 }
